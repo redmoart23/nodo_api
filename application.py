4e5a9e77ea186e5a182dc55b96e52f595a6ec380
red <redmoart@gmail.com>
2023-06-25 19:40:56 -0500

    third commit

--- application.py
+++ application.py
@@ -37,7 +37,7 @@ keywords_list = []
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
 def token_required(f):
     @wraps(f)
